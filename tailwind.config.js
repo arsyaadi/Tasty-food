875f31e319eafb6940a37b4f3c5bb70bd0a6b3f8
arsyaadi <arsyaadi@gmail.com>
2022-04-26 22:19:01 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ibm' : ['IBM Plex Sans', 'sans-serif'],
+      },
+      colors: {
+        'coklat': '#BFA065',
+      }
+    },
+  },
+  plugins: [],
+}
